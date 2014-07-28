6a9ad0be3747961a8568d643f8cb86181c21bfa2
neto737 <tynhomello@gmail.com>
2014-07-28 18:13:28 -0300

    Cleaned up code.

--- Phoenix Emulator/Phoenix/Communication/Messages/Avatar/ChangeMottoMessageEvent.cs
+++ Phoenix Emulator/Phoenix/Communication/Messages/Avatar/ChangeMottoMessageEvent.cs
@@ -6,49 +6,49 @@ using Phoenix.HabboHotel.Rooms;
 using Phoenix.Storage;
 namespace Phoenix.Communication.Messages.Avatar
 {
-	internal class ChangeMottoMessageEvent : MessageEvent
-	{
-		public void parse(GameClient Session, ClientMessage Request)
-		{
-			string motto = PhoenixEnvironment.FilterInjectionChars(Request.PopFixedString());
-			if (motto.Length <= 50 && !(motto != ChatCommandHandler.ApplyWordFilter(motto)) && !(motto == Session.GetHabbo().Motto))
-			{
-				Session.GetHabbo().Motto = motto;
-				using (DatabaseClient adapter = PhoenixEnvironment.GetDatabase().GetClient())
-				{
-					adapter.AddParamWithValue("motto", motto);
-					adapter.ExecuteQuery("UPDATE users SET motto = @motto WHERE Id = '" + Session.GetHabbo().Id + "' LIMIT 1");
-				}
-				if (Session.GetHabbo().CurrentQuestId == 17)
-				{
-					PhoenixEnvironment.GetGame().GetQuestManager().ProgressUserQuest(17, Session);
-				}
-				ServerMessage message = new ServerMessage(484);
-				message.AppendInt32(-1);
-				message.AppendStringWithBreak(Session.GetHabbo().Motto);
-				Session.SendMessage(message);
-				if (Session.GetHabbo().InRoom)
-				{
-					Room currentRoom = Session.GetHabbo().CurrentRoom;
-					if (currentRoom == null)
-					{
-						return;
-					}
-					RoomUser roomUserByHabbo = currentRoom.GetRoomUserByHabbo(Session.GetHabbo().Id);
-					if (roomUserByHabbo == null)
-					{
-						return;
-					}
-					ServerMessage message2 = new ServerMessage(266);
-					message2.AppendInt32(roomUserByHabbo.VirtualId);
-					message2.AppendStringWithBreak(Session.GetHabbo().Look);
-					message2.AppendStringWithBreak(Session.GetHabbo().Gender.ToLower()); 
-					message2.AppendStringWithBreak(Session.GetHabbo().Motto);
-					message2.AppendInt32(Session.GetHabbo().AchievementScore);
-					message2.AppendStringWithBreak("");
-					currentRoom.SendMessage(message2, null);
-				}
-				PhoenixEnvironment.GetGame().GetAchievementManager().UnlockAchievement(Session, 5, 1);
+    internal class ChangeMottoMessageEvent : MessageEvent
+    {
+        public void parse(GameClient Session, ClientMessage Request)
+        {
+            string motto = PhoenixEnvironment.FilterInjectionChars(Request.PopFixedString());
+            if (motto.Length <= 50 && !(motto != ChatCommandHandler.ApplyWordFilter(motto)) && !(motto == Session.GetHabbo().Motto))
+            {
+                Session.GetHabbo().Motto = motto;
+                using (DatabaseClient adapter = PhoenixEnvironment.GetDatabase().GetClient())
+                {
+                    adapter.AddParamWithValue("motto", motto);
+                    adapter.ExecuteQuery("UPDATE users SET motto = @motto WHERE Id = '" + Session.GetHabbo().Id + "' LIMIT 1");
+                }
+                if (Session.GetHabbo().CurrentQuestId == 17)
+                {
+                    PhoenixEnvironment.GetGame().GetQuestManager().ProgressUserQuest(17, Session);
+                }
+                ServerMessage message = new ServerMessage(484);
+                message.AppendInt32(-1);
+                message.AppendStringWithBreak(Session.GetHabbo().Motto);
+                Session.SendMessage(message);
+                if (Session.GetHabbo().InRoom)
+                {
+                    Room currentRoom = Session.GetHabbo().CurrentRoom;
+                    if (currentRoom == null)
+                    {
+                        return;
+                    }
+                    RoomUser roomUserByHabbo = currentRoom.GetRoomUserByHabbo(Session.GetHabbo().Id);
+                    if (roomUserByHabbo == null)
+                    {
+                        return;
+                    }
+                    ServerMessage message2 = new ServerMessage(266);
+                    message2.AppendInt32(roomUserByHabbo.VirtualId);
+                    message2.AppendStringWithBreak(Session.GetHabbo().Look);
+                    message2.AppendStringWithBreak(Session.GetHabbo().Gender.ToLower());
+                    message2.AppendStringWithBreak(Session.GetHabbo().Motto);
+                    message2.AppendInt32(Session.GetHabbo().AchievementScore);
+                    message2.AppendStringWithBreak("");
+                    currentRoom.SendMessage(message2, null);
+                }
+                PhoenixEnvironment.GetGame().GetAchievementManager().UnlockAchievement(Session, 5, 1);
                 if (Session.GetHabbo().FriendStreamEnabled)
                 {
                     using (DatabaseClient adapter = PhoenixEnvironment.GetDatabase().GetClient())
@@ -59,7 +59,7 @@ namespace Phoenix.Communication.Messages.Avatar
                         adapter.ExecuteQuery("INSERT INTO `friend_stream` (`id`, `type`, `userid`, `gender`, `look`, `time`, `data`) VALUES (NULL, '3', '" + Session.GetHabbo().Id + "', '" + Session.GetHabbo().Gender + "', @look, UNIX_TIMESTAMP(), @motto);");
                     }
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 }
--- Phoenix Emulator/Phoenix/Communication/Messages/Avatar/SaveWardrobeOutfitMessageEvent.cs
+++ Phoenix Emulator/Phoenix/Communication/Messages/Avatar/SaveWardrobeOutfitMessageEvent.cs
@@ -5,29 +5,29 @@ using Phoenix.Messages;
 using Phoenix.Storage;
 namespace Phoenix.Communication.Messages.Avatar
 {
-	internal class SaveWardrobeOutfitMessageEvent : MessageEvent
-	{
-		public void parse(GameClient Session, ClientMessage Request)
-		{
-			uint Slot = Request.PopWiredUInt();
-			string Look = Request.PopFixedString();
-			string Gender = Request.PopFixedString();
+    internal class SaveWardrobeOutfitMessageEvent : MessageEvent
+    {
+        public void parse(GameClient Session, ClientMessage Request)
+        {
+            uint Slot = Request.PopWiredUInt();
+            string Look = Request.PopFixedString();
+            string Gender = Request.PopFixedString();
 
-			using (DatabaseClient dbClient = PhoenixEnvironment.GetDatabase().GetClient())
-			{
-				dbClient.AddParamWithValue("userid", Session.GetHabbo().Id);
-				dbClient.AddParamWithValue("slotid", Slot);
-				dbClient.AddParamWithValue("look", Look);
-				dbClient.AddParamWithValue("gender", Gender.ToUpper());
-				if (dbClient.ReadDataRow("SELECT null FROM user_wardrobe WHERE user_id = @userid AND slot_id = @slotid LIMIT 1") != null)
-				{
-					dbClient.ExecuteQuery("UPDATE user_wardrobe SET look = @look, gender = @gender WHERE user_id = @userid AND slot_id = @slotid LIMIT 1;");
-				}
-				else
-				{
-					dbClient.ExecuteQuery("INSERT INTO user_wardrobe (user_id,slot_id,look,gender) VALUES (@userid,@slotid,@look,@gender)");
-				}
-			}
-		}
-	}
+            using (DatabaseClient dbClient = PhoenixEnvironment.GetDatabase().GetClient())
+            {
+                dbClient.AddParamWithValue("userid", Session.GetHabbo().Id);
+                dbClient.AddParamWithValue("slotid", Slot);
+                dbClient.AddParamWithValue("look", Look);
+                dbClient.AddParamWithValue("gender", Gender.ToUpper());
+                if (dbClient.ReadDataRow("SELECT null FROM user_wardrobe WHERE user_id = @userid AND slot_id = @slotid LIMIT 1") != null)
+                {
+                    dbClient.ExecuteQuery("UPDATE user_wardrobe SET look = @look, gender = @gender WHERE user_id = @userid AND slot_id = @slotid LIMIT 1;");
+                }
+                else
+                {
+                    dbClient.ExecuteQuery("INSERT INTO user_wardrobe (user_id,slot_id,look,gender) VALUES (@userid,@slotid,@look,@gender)");
+                }
+            }
+        }
+    }
 }
